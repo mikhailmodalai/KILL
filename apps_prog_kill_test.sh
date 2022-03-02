6d655a89cd4104bc2f87310b3ada5058b023271c
Mikhail <mikhail.rossoshanskiy@modalai.com>
2022-03-02 11:45:19 -0800

    files

---
+++ apps_prog_kill_test.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+adb shell "echo 8 > /proc/sys/kernel/printk"
+
+adb shell "echo 0 > /proc/sys/kernel/panic"
+
+adb shell "echo 1 > /sys/devices/platform/soc/5c00000.qcom,ssc/subsys7/keep_alive"
+
+adb shell "echo 0 >/sys/devices/system/cpu/cpu1/online"
+
+adb shell "echo 0 >/sys/devices/system/cpu/cpu2/online"
+
+adb shell "echo 0 >/sys/devices/system/cpu/cpu3/online"
+
+adb shell "echo 0 >/sys/devices/system/cpu/cpu4/online"
+
+adb shell "echo 0 >/sys/devices/system/cpu/cpu5/online"
+
+adb shell "echo 0 >/sys/devices/system/cpu/cpu6/online"
+
+adb shell "echo 0 >/sys/devices/system/cpu/cpu7/online"
+
+adb shell "echo c >/proc/sysrq-trigger"
---
+++ apps_prog_test.py
@@ -0,0 +1,58 @@
+
+import serial
+import time 
+
+# import csv
+
+# # open the file in the write mode
+# f = open('/home/mikhail/Desktop/data.csv', 'w')
+
+# # create the csv writer
+# writer = csv.writer(f)
+
+port = '/dev/ttyUSB0'
+baud = 250000
+
+ser = serial.Serial(port, baud)  # open serial port
+
+byte_size = 1
+start_byte = "af"              # start byte defined in ESC protocol as 0xAF
+end_byte =  'ae'
+seconds = 2
+bl = []
+master_list = []
+
+start_time = round(time.time())
+x = ser.read(1)
+while True: 
+    # current_time = time.time()
+    # elapsed_time =current_time - start_time
+    # if elapsed_time > seconds:  # time out loop
+    #     break
+    s = ser.read(byte_size)
+    s = s.hex()    # stores byte into string list
+    if not s:
+        continue
+    if s == start_byte or not s:
+        bl = []
+        bl.append(s)
+        while True:
+            s = (ser.read(byte_size)).hex()    # stores byte into string list
+            bl.append(s)
+            #if s == start_byte:
+            if s == 'ae' or s == 'a8':
+                master_list.append(bl)
+                bl = []
+                time.sleep(0.5) ##
+                for x in master_list:
+                    print(x)
+                break
+        else:
+            bl.append(s)
+
+for x in master_list:
+    print(x)
+# # write a row to the csv file
+# writer.writerow(master_list)
+
+ser.close()                     # close port 
